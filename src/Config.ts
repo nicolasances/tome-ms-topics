7b7ed49fecc1aa2f67d49f16c6bd1a18b6ebafd2
nicolasances <nicolas.matteazzi@gmail.com>
2025-12-27 19:05:11 +0100

    Fixed merge errors

--- src/Config.ts
+++ src/Config.ts
@@ -11,29 +11,8 @@ export class ControllerConfig extends TotoControllerConfig {
     mongoUser: string | undefined;
     mongoPwd: string | undefined;
 
-    constructor(configuration: ConfigurationData) {
-
-        super(configuration);
-
-    }
-
-    async load(): Promise<any> {
-        
-        let promises = [];
-
-        const secretsManager = new SecretsManager(this.hyperscaler == 'local' ? 'gcp' : this.hyperscaler, this.env, this.logger!);  // Use GCP Secrets Manager when local
-
-        promises.push(super.load());
-
-        promises.push(secretsManager.getSecret('tome-ms-topics-mongo-user').then((value) => {
-            this.mongoUser = value;
-        }));
-        promises.push(secretsManager.getSecret('tome-ms-topics-mongo-pswd').then((value) => {
-            this.mongoPwd = value;
-        }));
-
-        await Promise.all(promises);
-
+    getMongoSecretNames(): { userSecretName: string; pwdSecretName: string; } | null {
+        return { userSecretName: 'tome-ms-topics-mongo-user', pwdSecretName: 'tome-ms-topics-mongo-pswd' };
     }
 
     getProps(): APIOptions {
